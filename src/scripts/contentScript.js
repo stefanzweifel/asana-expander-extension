4e381c30957e3b94b412048b13fb90549ea8e5dc
Stefan Zweifel <hello@stefanzweifel.io>
2020-09-30 20:07:04 +0200

    Disable Expansion on Inbox

--- src/scripts/contentScript.js
+++ src/scripts/contentScript.js
@@ -2,7 +2,17 @@ import browser from 'webextension-polyfill';
 import {expandRichText, expandStoryFeed} from './expandContent.js';
 
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
     if (request.message === 'url-changed') {
+
+        // Prevent the extension from expanding links when
+        // looking at tasks in the inbox.
+        // (Expanding links in the inbox leads to an infinite scrolling loop)
+        if (new String(window.location).match(/inbox/) !== null) {
+            console.log('::asana-expander::doNothing');
+            return;
+        }
+
         expandStoryFeed();
         expandRichText();
     }
